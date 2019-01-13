7744dd8ab4bab51ac4087c1631bd07af55dc9bbc
ianpano@gmail.com <ianpano@gmail.com>
2019-01-13 21:12:57 +0800

    Finished Chapter - Create Complex Data Model

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +11,22 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [StringLength(50, MinimumLength =1, ErrorMessage ="First name cannot be longer than 50 characters")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$", ErrorMessage ="Must start with an Upper case letter, and all must be alpha characters")]
+        [Column("FirstName")]
+        [Required]
+        [Display(Name ="First Name")]
         public string FirstMidName { get; set; }
-        public DateTime EnrollmentDate { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]        
+        public DateTime EnrollmentDate { get; set; }
+        [Display(Name ="Full Name")]
+        public string FullName { get { return LastName + " " + FirstMidName; } }
         public ICollection<Enrollment> Enrollments { get; set; }
 
 
